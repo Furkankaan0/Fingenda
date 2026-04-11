f12ef6f66367b71b23a4ede6000a9f794cadfee0
Furkan Pekericli <246955490+Furkannkaan0@users.noreply.github.com>
2026-04-11 22:02:44 +0300

    Use blank native iOS launch handoff

--- scripts/generate-ios-launchscreen.js
+++ scripts/generate-ios-launchscreen.js
@@ -7,37 +7,15 @@ const ASSETS_DIR = path.join(IOS_APP_DIR, 'Assets.xcassets');
 const SPLASH_IMAGESET_DIR = path.join(ASSETS_DIR, 'Splash.imageset');
 const STORYBOARD_DIR = path.join(IOS_APP_DIR, 'Base.lproj');
 const STORYBOARD_PATH = path.join(STORYBOARD_DIR, 'LaunchScreen.storyboard');
-const SPLASH_SOURCE = path.join(ROOT, 'icons', 'icon-512x512.png');
 
 function ensureDir(dir) {
   fs.mkdirSync(dir, { recursive: true });
 }
 
-function writeSplashAsset() {
-  if (!fs.existsSync(SPLASH_SOURCE)) {
-    throw new Error(`Missing launch logo source: ${SPLASH_SOURCE}`);
+function removeLegacySplashAsset() {
+  if (fs.existsSync(SPLASH_IMAGESET_DIR)) {
+    fs.rmSync(SPLASH_IMAGESET_DIR, { recursive: true, force: true });
   }
-
-  ensureDir(SPLASH_IMAGESET_DIR);
-
-  fs.copyFileSync(SPLASH_SOURCE, path.join(SPLASH_IMAGESET_DIR, 'splash.png'));
-  fs.writeFileSync(
-    path.join(SPLASH_IMAGESET_DIR, 'Contents.json'),
-    JSON.stringify({
-      images: [
-        {
-          filename: 'splash.png',
-          idiom: 'universal',
-          scale: '1x'
-        }
-      ],
-      info: {
-        author: 'xcode',
-        version: 1
-      }
-    }, null, 2) + '\n',
-    'utf8'
-  );
 }
 
 function writeLaunchStoryboard() {
@@ -58,21 +36,8 @@ function writeLaunchStoryboard() {
                     <view key="view" contentMode="scaleToFill" id="FingendaLaunchView">
                         <rect key="frame" x="0.0" y="0.0" width="393" height="852"/>
                         <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
-                        <subviews>
-                            <imageView clipsSubviews="YES" userInteractionEnabled="NO" contentMode="scaleAspectFit" image="Splash" translatesAutoresizingMaskIntoConstraints="NO" id="FingendaLaunchLogo">
-                                <rect key="frame" x="130.5" y="360" width="132" height="132"/>
-                                <constraints>
-                                    <constraint firstAttribute="width" constant="132" id="FingendaLaunchLogoWidth"/>
-                                    <constraint firstAttribute="height" constant="132" id="FingendaLaunchLogoHeight"/>
-                                </constraints>
-                            </imageView>
-                        </subviews>
                         <viewLayoutGuide key="safeArea" id="FingendaLaunchSafeArea"/>
                         <color key="backgroundColor" red="0.9568627451" green="0.9647058824" blue="0.9764705882" alpha="1" colorSpace="custom" customColorSpace="sRGB"/>
-                        <constraints>
-                            <constraint firstItem="FingendaLaunchLogo" firstAttribute="centerX" secondItem="FingendaLaunchView" secondAttribute="centerX" id="FingendaLaunchLogoCenterX"/>
-                            <constraint firstItem="FingendaLaunchLogo" firstAttribute="centerY" secondItem="FingendaLaunchView" secondAttribute="centerY" id="FingendaLaunchLogoCenterY"/>
-                        </constraints>
                     </view>
                 </viewController>
                 <placeholder placeholderIdentifier="IBFirstResponder" id="FingendaLaunchFirstResponder" sceneMemberID="firstResponder"/>
@@ -80,9 +45,6 @@ function writeLaunchStoryboard() {
             <point key="canvasLocation" x="0.0" y="0.0"/>
         </scene>
     </scenes>
-    <resources>
-        <image name="Splash" width="512" height="512"/>
-    </resources>
 </document>
 `;
 
@@ -95,9 +57,9 @@ function main() {
     return;
   }
 
-  writeSplashAsset();
+  removeLegacySplashAsset();
   writeLaunchStoryboard();
-  console.log('[iOS LaunchScreen] Fingenda launch screen generated.');
+  console.log('[iOS LaunchScreen] Blank native launch screen generated; web splash remains the only branded splash.');
 }
 
 main();
